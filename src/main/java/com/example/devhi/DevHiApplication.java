a063449c54c5f31cfc04a3a03d849eea4d339ed3
minkyoung kim <minry20415@naver.com>
2023-11-21 21:14:10 +0900

    :sparkles: first commit

---
+++ src/main/java/com/example/devhi/DevHiApplication.java
@@ -0,0 +1,13 @@
+package com.example.devhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevHiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devhi/DevHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
