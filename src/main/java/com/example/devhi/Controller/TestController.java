d5a2cf521c08e8479a5c40cf40e9da43dd6545be
minkyoung kim <minry20415@naver.com>
2023-11-22 21:33:49 +0900

    :sparkles: - Dockerfile 생성 - TestController 생성 - yml 수정

---
+++ src/main/java/com/example/devhi/Controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.devhi.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping(value = "/")
+	public String getHello() {
+
+		return "Hello!";
+	}
+}
